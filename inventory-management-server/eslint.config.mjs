09301c1289c3b5f61751ab1894c3d725b5fe524f
Ajmain-Fayek <ajmainfayek733@gmail.com>
2026-02-17 23:30:31 +0600

    project init

---
+++ inventory-management-server/eslint.config.mjs
@@ -0,0 +1,21 @@
+//@ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+
+export default defineConfig(
+  {
+    ignores: ["node_modules/**", "dist/**", "build/**", "generated/**"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.stylistic,
+  prettierConfig,
+  {
+    rules: {
+      "no-console": "warn",
+      indent: ["error", 2],
+    },
+  },
+);
