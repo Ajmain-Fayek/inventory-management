2fc7ed4ce8b03ff001c62894ee2773165fd681ff
Ajmain-Fayek <ajmainfayek733@gmail.com>
2026-03-11 01:23:45 +0600

    fix/cookie issue-v3

--- inventory-management/lib/auth-client.ts
+++ inventory-management/lib/auth-client.ts
@@ -1,10 +1,9 @@
 import { createAuthClient } from "better-auth/client";
-import { envConfig } from "@/config/envConfig";
 
 /**
  * Singleton better-auth client.
  * baseURL points to the BACKEND where better-auth is mounted.
  */
 export const authClient = createAuthClient({
-  baseURL: envConfig.BACKEND_BASE_URL,
+  baseURL: "/api/proxy",
 });
--- inventory-management/next.config.ts
+++ inventory-management/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config option here */
+  async rewrites() {
+    return [
+      {
+        source: "api/proxy/:path*",
+        destination: "https://inventory-management-server-gffg.onrender.com/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
